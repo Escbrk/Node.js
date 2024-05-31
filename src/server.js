cd24f2cde814b1bd3a876285602d623ece17c178
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-05-30 19:35:16 -0600

    upd

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { env } from './utils/env.js';
+
+dotenv.config();
 
 export const startServer = () => {
   const app = express();
@@ -25,14 +29,14 @@ export const startServer = () => {
 
   app.use((req, res) => {
     res.status(404).send('Oops! Route was not found!');
-    res.status(404).send('Oops! Route was not found!');
   });
 
   app.use((error, req, res) => {
     res.status(500).send(error.message);
   });
 
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  const PORT = env('PORT', 3000);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
   });
 };
