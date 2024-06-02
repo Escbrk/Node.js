0416a2cd3bf563c40d594a81e8be947ccb9503a5
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-06-02 14:02:11 -0600

    upd

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/index.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
-
-dotenv.config();
 
 export const startServer = () => {
   const app = express();
@@ -20,22 +17,32 @@ export const startServer = () => {
     }),
   );
 
+  app.use(express.json());
   app.use(cors());
 
+  // app.use('*', (req, res, next) => {
+  //   console.log(`Login time: ${new Date().toLocaleString()}`);
+  //   next();
+  // });
+
   app.get('/', (req, res) => {
-    res.send('Hello world!');
+    // res.send('Hello world!');
+    res.json({
+      message: 'Hello World!',
+    });
   });
 
   app.get('/error', (req, res, next) => {
     next(new Error('some error here'));
   });
 
-  app.use(notFoundMiddleware);
+  app.use('*', notFoundMiddleware);
 
   app.use(errorHandlerMiddleware);
 
-  const PORT = env(ENV_VARS.PORT, 3000);
+  const PORT = Number(env(ENV_VARS.PORT, 3000));
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
+    console.log(`Login time: ${new Date().toLocaleString()}`);
   });
 };
