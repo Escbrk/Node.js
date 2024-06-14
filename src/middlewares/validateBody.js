b0c71b28f2ffe7aa125e7f52794c61cf6da58eb3
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-06-13 23:21:02 -0600

    upd

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -4,14 +4,14 @@ export const validateBody = (schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req.body, {
       abortEarly: false,
+      convert: false,
     });
     next();
   } catch (err) {
     const errors = err.details.map((error) => error.message);
+
     next(
       createHttpError(400, 'Bad request', {
-        //   error: message,
-        // error: err.details[0].message,
         error: errors,
       }),
     );
