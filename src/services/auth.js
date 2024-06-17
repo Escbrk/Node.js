52619ec56d3d0d1ecb357cf7ff4e09a92a7fa83e
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-06-17 01:10:09 -0600

    upd

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,16 @@
 import createHttpError from 'http-errors';
 import { User } from '../db/models/user.js';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 export const createUser = async (payload) => {
   const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+  const user = await User.findOne({ email: payload.email });
+
+  if (user) {
+    throw createHttpError(409, 'User with this email is already in database');
+  }
 
   return await User.create({ ...payload, password: hashedPassword });
 };
@@ -15,5 +22,8 @@ export const loginUser = async ({ email, password }) => {
   const areEqual = await bcrypt.compare(password, user.password);
   if (!areEqual) throw createHttpError(401, 'Unautorized');
 
-  return user;
+  const accessToken = crypto.randomBytes(20).toString('base64');
+  const refreshToken = crypto.randomBytes(20).toString('base64');
+
+  return { accessToken, refreshToken };
 };
