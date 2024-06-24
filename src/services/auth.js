7a7a6b1525b5f183f9444de33f4b34aef92e6221
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-06-23 21:20:50 -0600

    upd

--- src/services/auth.js
+++ src/services/auth.js
@@ -15,7 +15,7 @@ const createSession = () => {
     accessTokenValidUntil: Date.now() + TOKENS_PERION.FIFTEEN_MINUTES,
     refreshTokenValidUntil: Date.now() + TOKENS_PERION.SEVEN_DAYS,
   };
-}
+};
 
 export const createUser = async (payload) => {
   const hashedPassword = await bcrypt.hash(payload.password, 10);
@@ -79,20 +79,23 @@ export const requestResetToken = async (email) => {
   const user = await User.findOne({ email });
   if (!user) throw createHttpError(404, 'User not found!');
 
-  const resetToken = jwt.sign({
-    sub: user._id,
-    email,
-  }, {
-    // env('JWT_SECRET'),
-  }, {
-    expiresIn: '15m'
-  });
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    {
+      // env('JWT_SECRET'),
+    },
+    {
+      expiresIn: '15m',
+    },
+  );
 
   await sendEmail({
     from: env(SMTP.SMTP_FROM),
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href="${resetToken}">here</a> to reset your password</p>`
-  })
-
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password</p>`,
+  });
 };
