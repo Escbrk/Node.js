bfbab97c0954c1ad6b554dd044b49cc2a43f9807
Paul <69437294+Escbrk@users.noreply.github.com>
2024-06-27 13:43:41 -0600

    upd

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -6,12 +6,16 @@ import { saveToLocalMachine } from './saveToLocalMachine.js';
 export const saveFile = async (file) => {
   if (!file) return;
 
-  let url;
-  if (env(ENV_VARS.CLAUDINARY.IS_CLOUDINARY_ENABLED) === 'true') {
-    url = await saveToClaudinary(file);
-  } else {
-    url = await saveToLocalMachine(file);
-  }
+  // let url;
+  // if (env(ENV_VARS.CLAUDINARY.IS_CLOUDINARY_ENABLED) === 'true') {
+  //   url = await saveToClaudinary(file);
+  // } else {
+  //   url = await saveToLocalMachine(file);
+  // }
+  // return url
 
-  return url;
+  return env(ENV_VARS.CLAUDINARY.IS_CLOUDINARY_ENABLED) === 'true'
+      ? await saveToClaudinary(file)
+      : await saveToLocalMachine(file);
+
 };
